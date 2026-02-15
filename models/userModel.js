c5b8f1bd76a2f04f209ec53c0650440cedb0a254
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-02-14 23:18:38 -0700

    Create userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,51 +1,37 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../config/prisma.js";
 
-// Create a new user
-async function createUser({
-	username,
-	password,
-	firstname,
-	lastname,
-	role = "USER",
-	avatarUrl,
-}) {
+async function createUser({ username, password, role = "USER" }) {
 	const hashedPassword = await bcrypt.hash(password, 10);
 
 	return prisma.user.create({
 		data: {
 			username,
 			password: hashedPassword,
-			firstname,
-			lastname,
 			role,
-			avatarUrl,
 		},
 	});
 }
 
-// Find user by username
 async function findByUsername(username) {
 	return prisma.user.findUnique({
 		where: { username },
 	});
 }
 
-// Find user by id
 async function findById(id) {
 	return prisma.user.findUnique({
 		where: { id },
 	});
 }
 
-// Validate user password
 async function validatePassword(user, password) {
 	return bcrypt.compare(password, user.password);
 }
 
-// Remove password field before returning user
 function sanitizeUser(user) {
 	if (!user) return null;
+
 	const { password, ...safeUser } = user;
 	return safeUser;
 }
